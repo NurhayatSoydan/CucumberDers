df6e73d80312e9c9a1c6aa23cb36ba00518cf64d
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-11-07 20:03:53 +0300

    jenkins son hali

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -22,11 +22,11 @@ public class Hooks {
         ExcelUtility.writeToExcel("", senaryo);
 
         // extent reportun plugini aktif iken açık kalsın
-        if (senaryo.isFailed()){
+   /*     if (senaryo.isFailed()){
             TakesScreenshot ts=((TakesScreenshot) GWD.getDriver());
             byte[] hafizadakiHali=ts.getScreenshotAs(OutputType.BYTES);
             senaryo.attach(hafizadakiHali, "image/png", "screenshot name");
-        }
+        }*/
 
         //System.out.println("Senaryo bitti ?");
         // doğru çalışırsa burada quiti çağıracağım
